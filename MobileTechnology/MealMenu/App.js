cd821f084d5dbcdce69ad7408214cee534cedae5
koosenmongol <koosenmongol@gmail.com>
2025-11-13 11:39:15 +0800

    11.13

--- MobileTechnology/MealMenu/App.js
+++ MobileTechnology/MealMenu/App.js
@@ -1,18 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigation } from "@react-navigation/stack";
+
 import CatergoriesScreen from "./Screen/CatergoriesScreen";
 import MealsOverviewScreen from "./Screen/MealsOverviewScreen";
-const Stack = createNativeStackNavigation();
+
+// const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="MealsCategories" component={CatergoriesScreen} />
+    //     <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Screen name="MealsCategories" component={CatergoriesScreen} />
-        <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-      </NavigationContainer>
+      <CatergoriesScreen />
     </View>
   );
 }
